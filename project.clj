5e5a23ee2319349910a20fceabcf6a61c27731fe
Rodrigo <tsukigva@gmail.com>
2021-01-27 20:02:05 -0300

    added first functions and starting to implement repl

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject tuf "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject tuf "0.1.0"
+  :description "the stack stuff programming language"
+  :url "https://github.com/TsukiGva2/tuf-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot tuf.core
-  :target-path "target/%s"
+  :target-path "bin/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/tuf/core.clj
+++ src/tuf/core.clj
@@ -2,14 +2,31 @@
   (:gen-class))
 
 (defn lifo-into
+  "put a vector at the beginning of another vector"
   [target vect]
   (flatten (cons vect target)))
 
+(def PARSE-ERR 1)
 
+(def functions
+  #{:print #(println %)
+    :push (fn [stack value] (lifo-into stack value))
+    })
+
+(defn tufparse
+  [expr]
+)
+
+(defn tufval
+  [expr]
+  (tufrun (if (= (tufparse expr) PARSE-ERR)
+            #{:print "couldn't parse expression"}
+            (tufparse expr))))
 
 (defn -main
   "tuf repl"
   [& args]
-  (print "<tuf->")
-  (flush)
-  (tufval (read-line)))
+  (loop []
+    (print "<tuf->")
+    (flush)
+    (if (= (tufval (read-line)) "exit") nil (recur))))
